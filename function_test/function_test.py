42e989316ce85547e0f51b54d758a0755f05acdf
xinpaladin <7850151@163.com>
2019-06-03 22:21:44 +0800

    重构code

--- function_test/function_test.py
+++ function_test/function_test.py
@@ -1,17 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
 import time
 import unittest
-
-
-# browser = webdriver.Firefox()
-# browser.get('http://localhost:8000')
-
-# # assert 'Django' in browser.title
-# assert 'To-Do' in browser.title, 'Browser title was %s' % browser.title
-
-# browser.quit()
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -21,37 +11,62 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_adn_retrieve_it_later(self):
-        # 听说一个很酷的在线待办事项应用
-        # 看首页
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get('http://localhost:8000')
 
-        # 注意到 标题和头部都包含"To-Do"这个词
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        headers_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', headers_text)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # 输入待办事项
+        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.fail('Finish the test')
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+        self.fail('Finish the test!')
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
 
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-    # unittest.main()
