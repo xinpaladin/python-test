a4e7434a5a6f622c4632248bfa54d7641df54a9a
张鑫 <zhangx016@foresealife.com>
2019-06-03 11:41:21 +0800

    functional test now checks we can input

--- function_test/function_test.py
+++ function_test/function_test.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import time
 import unittest
 
 
@@ -19,10 +22,27 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_adn_retrieve_it_later(self):
+        # 听说一个很酷的在线待办事项应用
+        # 看首页
         self.browser.get('http://localhost:8000')
 
-        #
+        # 注意到 标题和头部都包含"To-Do"这个词
         self.assertIn('To-Do', self.browser.title)
+        headers_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', headers_text)
+
+        # 输入待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         self.fail('Finish the test')
 
 
