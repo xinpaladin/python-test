3d1690674ebab057820ec315b72e15b0b98704a8
xinpaladin <7850151@163.com>
2019-06-03 21:24:10 +0800

    Refactor hoime page view to use a template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,15 +1,17 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
+
 # Create your tests here.
 
 
 class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+    # def test_bad_maths(self):
+    #     self.assertEqual(1+1, 3)
 
     def test_root_url_resolve_to_home_page_view(self):
         found = resolve('/')
@@ -19,6 +21,14 @@ class SmokeTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title> To-Do lists </title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        # self.assertTrue(html.startswith('<html>'))
+        # self.assertIn('<title>To-Do lists</title>', html)
+        # self.assertTrue(html.endswith('</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
+
+    def test_django_home_page_returns_correct_html(self):
+        
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+        
